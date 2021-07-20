e6618e91c2443ff72859cb62121b28fd67f59119
domalasherpa <domalasherpa9813@gmail.com>
2021-07-20 12:29:24 +0545

    dictionay

--- dict.py
+++ dict.py
@@ -32,20 +32,26 @@ print(point)
 #Generate a list of usernames, name, age and poison from the above dictionary.
 #Take the above list and put it in order.
 
-users = {'g91':{'name':'Aron','age':55,'poison':'Old monk'},
-       'twir56':{'name':'Visakha','age':26,'poison':'coca cola'},
-       'jsdl8':{'name':'Saudi','age':12,'poison':'hinwa'}}
+users = {'g91': {'name': 'Aron', 'age': 55, 'poison': 'Old monk'},
+         'twir56':{'name':'Visakha','age':26,'poison':'coca cola'},
+         'jsdl8':{'name':'Saudi','age':12,'poison':'hinwa'}}
 username = []
-values = {}
-
+values = []
+name = []
+age = []
+poison = []
 for key, value in users.items():
     username.append(key)
     values.append(value)
 
-print(values)
+for i in range(len(values)):
+    name.append(values[i]['name'])
+    age.append(values[i]['age'])
+    poison.append(values[i]['poison'])
 
-
-
+print(name)
+print(age)
+print(poison)
 
 
 #Create an empty dictionary called milk_carton. Add the following key/value pairs.You can make up the values or use a real milk carton.
@@ -55,12 +61,15 @@ print(values)
 #Brand_name
 #Print out the values of all of the elements of the milk_carton using the values in the dictionary.
 #Show how to calculate the cost of six cartons of milk based on the cost of the milk_carton.
-
 milk_carton = {}
-milk_carton['Expiration_date'] = ('day', 'month', 'year')
+milk_carton['Expiration_date'] = (18, 'January', 2021)
 milk_carton['Volume'] = '40 ml'
-milk_carton['Cost'] = '40'
+milk_carton['Cost'] = 40
 milk_carton['Brand_name'] = 'DDC'
-print(milk_carton)
+
 print(milk_carton.values())
-print("Cost of six milk_carton = ", 6 * milk_carton['cost'])
+
+no_of_carton = int(input("Enter the number of the milk carton: "))
+cost = no_of_carton * milk_carton['Cost']
+
+print(f"Cost of {no_of_carton} is Rs.{cost}.")
